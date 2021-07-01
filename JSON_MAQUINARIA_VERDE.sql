1c80b6fda7f7ce57f0a9fa9d2a28b5ce5ef0d0f0
Jhon C <jhon11421@gmail.com>
2021-07-01 09:50:55 -0500

    SQL para JSON de MAQUINARIA VERDE

---
+++ JSON_MAQUINARIA_VERDE.sql
@@ -0,0 +1,16 @@
+SELECT
+	id_etp_produccion,
+	JSON_ARRAYAGG(json_object("tipo_maquinaria",
+	CAST(tipo AS SIGNED),
+	"estado",
+	CAST(estado AS SIGNED),
+	"acceso",
+	CAST(acceso AS SIGNED),
+	"suficiencia",
+	CAST(suficiencia AS SIGNED),
+	"ubicacion",
+	ubicacion))
+FROM
+	maquinaria_verde
+GROUP BY
+	id_etp_produccion
