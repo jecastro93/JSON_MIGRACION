d08f1bcb70e43d1fe4022dd761d470166d29d530
Jhon C <jhon11421@gmail.com>
2021-08-03 15:21:01 -0500

    SQL para JSON de PROBLEMAS Y OPORTUNIDADES

--- JSON_PROBLEMAS_OPORTUNIDADES.sql
+++ JSON_PROBLEMAS_OPORTUNIDADES.sql
@@ -1,16 +1,27 @@
 SELECT
 	id_ficha,
-	JSON_ARRAYAGG(json_object("tipo_prpyecto",
-	CAST(tipo AS SIGNED),
-	"entidad_responsable",
-	entidad_responsable,
-	"objeto",
-	objeto,
-	"valor",
-	CAST(valor AS SIGNED),
-	"num_beneficiario",
-	CAST(numero_beneficiarios AS SIGNED)))
+	JSON_ARRAYAGG(json_object("tipo",
+	CAST(tipo_retos_oportunidades AS SIGNED),
+	"componente",
+	componente,
+	"principal_problemas",
+	CASE
+		WHEN reto IS null THEN null
+		else JSON_EXTRACT (CONCAT("[", reto, "]"),
+		"$")
+	END,
+	"otro_problema",
+	otro,
+	"principal_oportunidad",
+	CASE
+		WHEN oportunidad IS null THEN null
+		else JSON_EXTRACT (CONCAT("[", oportunidad, "]"),
+		"$")
+	END,
+	"otra_oportunidad",
+	otro))
 FROM
-	proyectos
+	retos_oportunidades
+WHERE id_ficha IN (SELECT cod_unico_mesa FROM mesasxlinea_productiva WHERE id_grupo_ficha = 1 OR id_grupo_ficha = 2)
 GROUP BY
 	id_ficha
